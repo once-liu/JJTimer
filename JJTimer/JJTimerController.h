2abcbaac17cf649adfa74b6aa70d1aadf14cde41
lzc <1244315824@qq.com>
2020-11-25 22:14:08 +0800

    Retain Cycle 演示

---
+++ JJTimer/JJTimerController.h
@@ -0,0 +1,16 @@
+//
+//  JJTimerController.h
+//  JJTimer
+//
+//  Created by LZC on 2020/11/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JJTimerController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JJTimer/JJTimerController.m
@@ -0,0 +1,73 @@
+//
+//  JJTimerController.m
+//  JJTimer
+//
+//  Created by LZC on 2020/11/25.
+//
+
+#import "JJTimerController.h"
+#import "JJTimer.h"
+
+@interface JJTimerController ()
+
+@property (nonatomic, strong) NSTimer *nsTimer;
+
+@end
+
+@implementation JJTimerController
+
+- (void)dealloc {
+    NSLog(@" - dealloc - ");
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    //FIXME: NSTimer 过渡解决方案
+}
+
+
+- (IBAction)startAction:(id)sender {
+    //FIXME: 通过 identifier， 停止多个开启的 timer
+    [JJTimer jj_scheduledTimerWithAsync:YES interval:1.5 delay:0 repeats:YES block:^{
+        NSLog(@" - 1 - thread: %@", [NSThread currentThread]);
+    }];
+    
+    [JJTimer jj_scheduledTimerWithAsync:YES interval:1.5 delay:0 repeats:YES block:^{
+        NSLog(@" - 2 - thread: %@", [NSThread currentThread]);
+    }];
+}
+
+- (IBAction)endAction:(id)sender {
+    [JJTimer jj_invalidate];
+    
+    [self.nsTimer invalidate];
+}
+
+
+
+- (void)nsTiemerDemo {
+    /**
+    // 1.
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+        NSLog(@" - %s", __func__);
+    }];
+    self.nsTimer = timer;
+     */
+    
+    // 2.
+    // NSTimer 对 target 强引用。The timer maintains a strong reference to this object until it (the timer) is invalidated.
+    NSTimer *timer = [NSTimer timerWithTimeInterval:1 target:self selector:@selector(nsTimerAction:) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
+    self.nsTimer = timer;
+    
+//    [timer invalidate];
+}
+
+- (void)nsTimerAction:(NSTimer *)timer {
+    NSLog(@" - %s", __func__);
+}
+
+@end
